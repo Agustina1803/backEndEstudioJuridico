d684a69faca2c738d3aa1767d7245168e5d1bb2e
AgusBulacio <agusbulacio1803@gmail.com>
2025-12-08 23:45:35 -0300

    agrege el metodo para listar las citas por id y agrege la ruta para poder hacer la consulta

--- controllers/cita.controllers.js
+++ controllers/cita.controllers.js
@@ -1,29 +1,46 @@
 import Cita from "../models/cita.js";
 
-// FUNCION PARA CREAR UNA CITA
-export const crearCita = async (req, res) =>{
-try{
+
+export const crearCita = async (req, res) => {
+  try {
     const citaNueva = new Cita(req.body);
     await citaNueva.save();
     res.status(201).json({
-        mensaje: "Cita creada exitosamente"
+      mensaje: "Cita creada exitosamente",
     });
-}catch(error){
+  } catch (error) {
     console.error(error);
     res.status(500).json({
-        mensaje: "Error al crear la cita"
+      mensaje: "Error al crear la cita",
     });
-}
-}
+  }
+};
 
-// funcion para listar las citas
 
-export const listarCita = async (req, res) =>{
-    try{
-        const listaCita = await Cita.find();
-        res.status(200).json(listaCita);
-    }catch(error){
-        console.error(error);
-        res.status(500).json({mensaje:"Error al obtener los productos"})
+export const listarCita = async (req, res) => {
+  try {
+    const listaCita = await Cita.find();
+    res.status(200).json(listaCita);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al obtener los productos" });
+  }
+};
+
+
+export const listarCitaId = async (req, res) => {
+  try {
+    const listarCitaId = await Cita.findById(req.params.id);
+    if (!listarCitaId) {
+      return res
+        .status(404)
+        .json({ mensaje: "El producto con ese ID no existe" });
     }
-}
+    res.status(200).json(listarCitaId);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al obtener el producto por ID" });
+  }
+};
+
+
--- routes/cita.routes.js
+++ routes/cita.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { crearCita, listarCita } from "../controllers/cita.controllers.js";
+import { crearCita, listarCita, listarCitaId } from "../controllers/cita.controllers.js";
 
 
 const router = Router();
 
 router.route("/").post(crearCita).get(listarCita);
+router.route("/:id").get(listarCitaId);
 
 export default router;
