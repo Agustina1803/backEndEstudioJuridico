260e6bb6ef1ab0267ceea08de8ef467954097cab
AgusBulacio <agusbulacio1803@gmail.com>
2025-12-09 00:04:32 -0300

    agrege la funcion para editar la cita y la ruta necesaria en cita.routers para hacer la consulta

--- controllers/cita.controllers.js
+++ controllers/cita.controllers.js
@@ -1,6 +1,5 @@
 import Cita from "../models/cita.js";
 
-
 export const crearCita = async (req, res) => {
   try {
     const citaNueva = new Cita(req.body);
@@ -16,7 +15,6 @@ export const crearCita = async (req, res) => {
   }
 };
 
-
 export const listarCita = async (req, res) => {
   try {
     const listaCita = await Cita.find();
@@ -27,14 +25,11 @@ export const listarCita = async (req, res) => {
   }
 };
 
-
 export const listarCitaId = async (req, res) => {
   try {
     const listarCitaId = await Cita.findById(req.params.id);
     if (!listarCitaId) {
-      return res
-        .status(404)
-        .json({ mensaje: "La cita con ese ID no existe" });
+      return res.status(404).json({ mensaje: "La cita con ese ID no existe" });
     }
     res.status(200).json(listarCitaId);
   } catch (error) {
@@ -43,15 +38,31 @@ export const listarCitaId = async (req, res) => {
   }
 };
 
-export const borrarCitaId = async (req, res) =>{
-    try{
-        const citaBorrada = await Cita.findByIdAndDelete(req.params.id);
-        if(!citaBorrada){
-            return res.status(404).json({mensaje: "La cita con ese ID no existe"});
-        }
-        res.status(200).json({mensaje: "La cita fue borrada con exito"});
-    }catch(error){
-        console.error(error);
-        res.status(500).json({mensaje: "Error al borrar la cita por ID "})
+export const borrarCitaId = async (req, res) => {
+  try {
+    const citaBorrada = await Cita.findByIdAndDelete(req.params.id);
+    if (!citaBorrada) {
+      return res.status(404).json({ mensaje: "La cita con ese ID no existe" });
     }
-}
+    res.status(200).json({ mensaje: "La cita fue borrada con éxito" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al borrar la cita por ID " });
+  }
+};
+
+export const editarCitaId = async (req, res) => {
+  try {
+    const citaEditada = await Cita.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!citaEditada) {
+      return res.status(404).json({ mensaje: "La cita con ese ID no existe" });
+    }
+    res.status(200).json({ mensaje: "Cita editada con éxito" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Erorr al actualizar la cita por ID" });
+  }
+};
--- routes/cita.routes.js
+++ routes/cita.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { borrarCitaId, crearCita, listarCita, listarCitaId } from "../controllers/cita.controllers.js";
+import { borrarCitaId, crearCita, editarCitaId, listarCita, listarCitaId } from "../controllers/cita.controllers.js";
 
 
 const router = Router();
 
 router.route("/").post(crearCita).get(listarCita);
-router.route("/:id").get(listarCitaId).delete(borrarCitaId);
+router.route("/:id").get(listarCitaId).delete(borrarCitaId).put(editarCitaId);
 
 export default router;
