e71754e19d2879bcd3b44f0ddd2a62b0278ad3df
daiana varela <dai.varela01@gmail.com>
2025-12-09 20:31:04 -0300

    agregue metodo put/update

--- routes/subirArchivo.routes.js
+++ routes/subirArchivo.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { crearSubirArchivo,listaSubirArchivo,obtenerSubirArchivoPorId,eliminarSubirArchivo} from "../controllers/subirArchivo.controllers.js";
+import { crearSubirArchivo,listaSubirArchivo,obtenerSubirArchivoPorId,eliminarSubirArchivo,editarSubirArchivo} from "../controllers/subirArchivo.controllers.js";
 
 
 const router = Router();
 router.route("/").post(crearSubirArchivo).get(listaSubirArchivo);
-router.route("/:id").get(obtenerSubirArchivoPorId).delete(eliminarSubirArchivo);
+router.route("/:id").get(obtenerSubirArchivoPorId).delete(eliminarSubirArchivo).put(editarSubirArchivo);
 
 
 
