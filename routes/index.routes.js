ecbf8b46ec88ed023df596233f97d7136d2877dc
ANCoronel <adrian.coronel86@hotmail.com>
2025-12-09 22:38:48 -0300

    Se le dio formato al Documento

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,11 +3,16 @@ import citaRoutes from "./cita.routes.js";
 import clienteRouter from "./cliente.routes.js"
 import usuarioRouter from "./usuarios.routes.js"
 import juicioRouter from "./juicios.routes.js"
+
+
+
 const router = Router();
 
 router.use("/citas", citaRoutes )
 router.use("/clientes", clienteRouter)
 router.use("/usuarios", usuarioRouter);
-router.use("/juicios", juicioRouter);
 
+
+
+router.use("/juicios", juicioRouter);
 export default router;
