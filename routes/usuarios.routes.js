a766726e5e01048b5aede15b36d4d7cfc4aab183
AgusBulacio <agusbulacio1803@gmail.com>
2025-12-13 12:05:50 -0300

    corregi errores de tipeo de is por id y Errror por Error en el valudarUsuario y agrege en el get por id que se corrobore que el id es valido para mongo db

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -5,8 +5,8 @@ import validarIds from "../middlewares/validarIds.js"
 
 const router = Router();
 
-// Importar los controladores
 
 router.route('/').get(obtenerUsuarios).post([validacionUsuario], crearUsuario);
 router.route('/:id').get(validarIds ,obtenerUsuarioPorId).delete([validarIds] ,eliminarUsuario).put([validarIds, validacionUsuario] ,actualizarUsuario);
+
 export default router;
