1b401c655754d7c366c5e263a37fc7e0183a7ec0
AgusBulacio <agusbulacio1803@gmail.com>
2025-12-13 16:27:01 -0300

    agregue la importacion de mongoose para poder usar el objeto de abogado e importe tarea

--- middlewares/validarTarea.js
+++ middlewares/validarTarea.js
@@ -1,15 +1,10 @@
 import { body } from "express-validator";
 import resultadoValidacion from "./resultadoValidacion.js";
 import Usuario from "../models/usuario.js";
+import Tarea from "../models/tarea.js"
+import mongoose from "mongoose";
 
 const validacionTarea = [
-  body("descripcion")
-    .notEmpty()
-    .withMessage("La descripción de la tarea es obligatoria")
-    .isLength({ min: 10, max: 1000 })
-    .withMessage(
-      "La descipcion de la tarea debe tener como minimo 10 y como maximo 1000 caracteres"
-    ),
   body("abogado")
     .notEmpty()
     .withMessage("El responsable es obligatorio")
@@ -39,16 +34,37 @@ const validacionTarea = [
       }
       return true;
     }),
-    body("prioridad")
+  body("prioridad")
     .notEmpty()
     .withMessage("La prioridad de la tarea es obligatoria")
     .isIn(["alta", "media", "baja"]),
-    body("estado")
+  body("estado")
     .notEmpty()
     .withMessage("El estado de la tarea es obligatoria")
-    .isIn(["Pendiente","Proceso", "Completa","Cancelada","Reprogramada"]),
+    .isIn(["Pendiente", "Proceso", "Completada", "Cancelada", "Reprogramada"]),
+  body("descripcion")
+    .notEmpty()
+    .withMessage("La descripción de la tarea es obligatoria")
+    .isLength({ min: 10, max: 1000 })
+    .withMessage(
+      "La descipcion de la tarea debe tener como minimo 10 y como maximo 1000 caracteres"
+    )
+    .custom(async (valor, { req }) => {
+      const { abogado, fecha, prioridad, estado } = req.body;
+      const tareaExistente = await Tarea.findOne({
+        descripcion: valor,
+        abogado,
+        fecha,
+        prioridad,
+        estado,
+      });
+      if (tareaExistente) {
+        throw new Error("Ya existe una tarea con los mismos datos");
+      }
+      return true;
+    }),
 
-    (req, res, next) => resultadoValidacion(req, res, next)
+  (req, res, next) => resultadoValidacion(req, res, next),
 ];
 
 export default validacionTarea;
