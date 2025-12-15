600002c0f268f2434bce14e4562e3d8c071fafff
AgusBulacio <agusbulacio1803@gmail.com>
2025-12-15 11:24:52 -0300

    agregue la funcion para el logeo del usuario, para que desde el front se pueda enviar el email y password del usuario se verifique que sean correctas y se envie el nombre dle usuario y el token

--- controllers/usuarios.controllers.js
+++ controllers/usuarios.controllers.js
@@ -1,103 +1,129 @@
-import Usuario from '../models/usuario.js';
+import Usuario from "../models/usuario.js";
 import bycrypt from "bcrypt";
 
 //POST
 export const crearUsuario = async (req, res) => {
-    try {
-        const saltos = bycrypt.genSaltSync(10);
-        const passwordEncriptada = bycrypt.hashSync(
-            req.body.formBasicPassword,
-            saltos
-        );
-        req.body.formBasicPassword = passwordEncriptada;
-        const nuevoUsuario = new Usuario(req.body);
-        await nuevoUsuario.save();
-        res.status(201).json({
-            message: 'Usuario creado exitosamente',
-        });
-    }
-    catch (error) {
-        console.error(error);
-        res.status(500).json({
-            message: 'Error en el servidor al crear el usuario',
-        });
-    }
-}
+  try {
+    const saltos = bycrypt.genSaltSync(10);
+    const passwordEncriptada = bycrypt.hashSync(
+      req.body.formBasicPassword,
+      saltos
+    );
+    req.body.formBasicPassword = passwordEncriptada;
+    const nuevoUsuario = new Usuario(req.body);
+    await nuevoUsuario.save();
+    res.status(201).json({
+      message: "Usuario creado exitosamente",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Error en el servidor al crear el usuario",
+    });
+  }
+};
 
 //GET
 export const obtenerUsuarios = async (req, res) => {
-    try {
-        const usuarios = await Usuario.find();
-        res.status(200).json(usuarios)
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: 'Error en el servidor al obtener los usuarios',
-        });
-    }
- 
-}
+  try {
+    const usuarios = await Usuario.find();
+    res.status(200).json(usuarios);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error en el servidor al obtener los usuarios",
+    });
+  }
+};
 
 //GET by ID
 export const obtenerUsuarioPorId = async (req, res) => {
-    try {
-        const usuariosporID = await Usuario.findById(req.params.id);
-        if (!usuariosporID) {
-            return res.status(404).json({
-                message: 'Usuario no encontrado',
-            });
-        }
-        res.status(200).json(usuariosporID);
+  try {
+    const usuariosporID = await Usuario.findById(req.params.id);
+    if (!usuariosporID) {
+      return res.status(404).json({
+        message: "Usuario no encontrado",
+      });
+    }
+    res.status(200).json(usuariosporID);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error en el servidor al obtener el usuario por ID",
+    });
+  }
+};
 
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: 'Error en el servidor al obtener el usuario por ID',
-        });
-    }
-}
 // DeLETE
 export const eliminarUsuario = async (req, res) => {
-    try {
-const usurioBorrado = await Usuario.findByIdAndDelete(req.params.id);
-        if (!usurioBorrado) {
-            return res.status(404).json({
-                message: 'Usuario no encontrado',
-            });
-        }
-        res.status(200).json({
-            message: 'Usuario eliminado exitosamente',
-        });
-        
-    }catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: 'Error en el servidor al eliminar el usuario',
-        });
+  try {
+    const usurioBorrado = await Usuario.findByIdAndDelete(req.params.id);
+    if (!usurioBorrado) {
+      return res.status(404).json({
+        message: "Usuario no encontrado",
+      });
     }
-}
+    res.status(200).json({
+      message: "Usuario eliminado exitosamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error en el servidor al eliminar el usuario",
+    });
+  }
+};
 
 //UPDATE
 
 export const actualizarUsuario = async (req, res) => {
-    try {
-        const usuarioActualizado = await Usuario.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!usuarioActualizado) {
-            return res.status(404).json({
-                message: 'Usuario no encontrado',
-            });
-                }
-        res.status(200).json({
-            message: 'Usuario actualizado exitosamente',
-       
-        });
+  try {
+    const usuarioActualizado = await Usuario.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!usuarioActualizado) {
+      return res.status(404).json({
+        message: "Usuario no encontrado",
+      });
     }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: 'Error en el servidor al actualizar el usuario',
-        });
+    res.status(200).json({
+      message: "Usuario actualizado exitosamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Error en el servidor al actualizar el usuario",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, formBasicPassword } = req.body;
+    const usuarioBuscado = await Usuario.findOne({
+      email: email,
+    });
+    if (!usuarioBuscado) {
+      return res.status(404).send("Uusario no encontrado");
     }
-}
+    const passwordCorrecto = bycrypt.compareSync(
+      formBasicPassword,
+      usuarioBuscado.formBasicPassword
+    );
+    if (!passwordCorrecto) {
+      return res.status(401).send("Credenciales incorrectas");
+    }
+    const token = await generarJWT(usuarioBuscado, passwordCorrecto);
+    res.status(200).json({
+      mensaje: "Logeo exitoso",
+      token: token,
+      nombre: usuarioBuscado.nombre,
+      role: usuarioBuscado.role,
+    });
+  } catch (erorr) {
+    console.error(error)
+    res.status(500).send("Error en el login del usuario");
+  }
+};
