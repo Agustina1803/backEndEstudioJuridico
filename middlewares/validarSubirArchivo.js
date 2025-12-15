1135f26570a4bd46c464bde82d87595ba7928d54
ANCoronel <adrian.coronel86@hotmail.com>
2025-12-15 15:12:01 -0300

    Se soluciona errores y se sigue trabajando en validarSubirArchivo

--- middlewares/validarSubirArchivo.js
+++ middlewares/validarSubirArchivo.js
@@ -2,15 +2,12 @@ import multer from 'multer';
 import { validationResult } from 'express-validator';
 import resultadoValidacion from "./resultadoValidacion.js";
 
-// Configuración de Multer (almacenamiento en memoria o disco)
 const storage = multer.memoryStorage();
 
 const validarsubirArchivos = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // límite de 5 MB
   fileFilter: (req, file, cb) => {
-    // Validar tipo de archivo
-    const archivosPermitidos = ['image/jpeg', 'image/png', 'application/pdf'];
+       const archivosPermitidos = ['image/jpeg', 'image/png', 'application/pdf'];
     if (!archivosPermitidos.includes(file.mimetype)) {
       return cb(new Error('Tipo de archivo no permitido'), false);
     }
@@ -18,7 +15,6 @@ const validarsubirArchivos = multer({
   }
 });
 
-// Middleware para validar errores de Multer y express-validator
 const validarArchivo = (req, res, next) => {
   const errors = validationResult(req);
 
@@ -33,7 +29,7 @@ const validarArchivo = (req, res, next) => {
   next();
 };
 
-export { validarsubirArchivos, validarArchivo };
+export default { validarsubirArchivos, validarArchivo };
 
 
 
