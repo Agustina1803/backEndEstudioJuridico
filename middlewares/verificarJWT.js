9dea26d86416a14eec7124a203e3769411d393a4
AgusBulacio <agusbulacio1803@gmail.com>
2025-12-15 11:07:36 -0300

    agregue la verificacion del token para comprobar que coinciden y que se envio el token correctmente

---
+++ middlewares/verificarJWT.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken"
+
+const verficarJWT = (req, res, next) =>{
+    try{
+        const token = req.headers["x-token"];
+        if(!token){
+            return res.status(401).json({mensaje:"No se envio un token"})
+        }
+        const payload = jwt.verify(token, process.env.SECRET_JWT);
+        req.email = payload.email;
+        req.formBasicPassword = payload.formBasicPassword;
+        req.role = payload.role;
+        next();
+    }catch(error){
+        console.error(error.mensaje);
+        res.status(401).json({mensaje:"Error al verificar el token"})
+    }
+}
+
+export default verficarJWT;
