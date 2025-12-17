345dc14474f5538191c3ad728b62a1f68f45da0e
AgusBulacio <agusbulacio1803@gmail.com>
2025-12-17 12:16:55 -0300

    se modifico el modelo de subirArchivo para modificar el atributo de seleccionarArchivo para configurar la url y el id publico

--- models/subirArchivo.js
+++ models/subirArchivo.js
@@ -1,29 +1,28 @@
 import mongoose, { Schema } from "mongoose";
-import validarSubirArchivo from '../middlewares/validarSubirArchivo.js';
-
-
 
 const subirArchivoSchema = new Schema({
-    nombreCliente: { type: String, required: true, maxLength: 50, minLength: 10, },
-    tipodearchivo: { type: String, required: true, enum: ['demanda', 'contrato', 'escrito', 'poder', 'notificacion'] },
-    fecha: {
-        type: String, // 👈 ahora se guarda como string
-        required: true,
-        validate: {
-            validator: function (value) {
-                // valida formato YYYY-MM-DD
-                return /^\d{4}-\d{2}-\d{2}$/.test(value);
-            },
-
-        },
+  nombreCliente: { type: String, required: true, maxLength: 50, minLength: 10 },
+  tipodearchivo: {
+    type: String,
+    required: true,
+    enum: ["demanda", "contrato", "escrito", "poder", "notificacion"],
+  },
+  fecha: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return /^\d{4}-\d{2}-\d{2}$/.test(value);
+      },
     },
+  },
 
-    seleccionarArchivo: {
-        type: String, // acá guardás la ruta o el nombre del archivo
-        required: [true, "El archivo es obligatorio"],
-    },
+  seleccionarArchivo: {
+    url: { type: String, required: true },
+    public_id: { type: String, required: true },
+  },
 });
 
-const SubirArchivo = mongoose.model('subirarchivo', subirArchivoSchema);
+const SubirArchivo = mongoose.model("subirarchivo", subirArchivoSchema);
 
 export default SubirArchivo;
