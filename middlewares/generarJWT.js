e6a3862cb3e370d93a327d6cad162e25ab54ec70
AgusBulacio <agusbulacio1803@gmail.com>
2025-12-17 21:49:33 -0300

    quite la contraseña del token

--- middlewares/generarJWT.js
+++ middlewares/generarJWT.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken"
 
-const generarJWT = (email, formBasicPassword, role) =>{
+const generarJWT = (email, role) =>{
 try{
-const payload = {email, formBasicPassword, role}
+const payload = {email, role}
 const token = jwt.sign(payload, process.env.SECRET_JWT, {
     expiresIn: "3h"
 });
