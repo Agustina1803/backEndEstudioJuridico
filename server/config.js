bc7105356c3ca47130814d05ba3d52c51bf6d848
AgusBulacio <agusbulacio1803@gmail.com>
2026-01-06 11:19:49 -0300

    configure la ruta del servidor para que no de 404 al acceder a la ruta /

--- server/config.js
+++ server/config.js
@@ -3,29 +3,30 @@ import cors from "cors";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-
-import '../database/config.js';
+import "../database/config.js";
 
 export default class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT || 3000;
-    this.middlewares(); 
+    this.middlewares();
   }
 
- 
   middlewares() {
-    this.app.use(cors()); 
-    this.app.use(morgan("dev")); 
-    this.app.use(express.json()); 
+    this.app.use(cors());
+    this.app.use(morgan("dev"));
+    this.app.use(express.json());
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
     console.log(__filename);
     console.log(__dirname);
-    this.app.use(express.static(path.join(__dirname, '../public')));
+    this.app.use(express.static(path.join(__dirname, "../public")));
+
+    this.app.get("/", (req, res) => {
+      res.send("Backend Estudio Jurídico funcionando");
+    });
   }
 
- 
   listen() {
     this.app.listen(this.port, () => {
       console.info(`server inciiado: http://127.0.0.1:${this.port}`);
